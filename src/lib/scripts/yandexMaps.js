a2b807143cc344e334de349da2fc4614b959d593
Горбулёв Сергей <gorbulevsv@yandex.ru>
2022-09-23 21:13:13 +0300

    Иниц-ия яндекс карт отправлена в отдельный файл

---
+++ src/lib/scripts/yandexMaps.js
@@ -0,0 +1,40 @@
+// @ts-nocheck
+export function yandexMaps(containerId = 'map', center = [55.751574, 37.573856], zoom = 10) {
+	return new Promise(function (resolve, reject) {
+		ymaps.ready(() => {
+			let myMap = new ymaps.Map(
+				containerId,
+				{
+					center: center,
+					zoom: zoom,
+					controls: []
+				},
+				{
+					searchControlProvider: 'yandex#search'
+				}
+			);
+			resolve(myMap);
+		});
+	});
+}
+
+export function yandexRoute(start, end, trafficJams = true) {
+	// Создание экземпляра маршрута.
+	var route = new ymaps.multiRouter.MultiRoute(
+		{
+			// Точки маршрута.
+			// Обязательное поле.
+			referencePoints: [start, end],
+			// Выбор маршрута с учётом пробок
+			params: {
+				avoidTrafficJams: trafficJams
+			}
+		},
+		{
+			// Автоматически устанавливать границы карты так,
+			// чтобы маршрут был виден целиком.
+			boundsAutoApply: true
+		}
+	);
+	return route;
+}
--- src/routes/distance/betweentwopoints/+page.svelte
+++ src/routes/distance/betweentwopoints/+page.svelte
@@ -2,60 +2,34 @@
 	// @ts-nocheck
 	import { onMount } from 'svelte';
 	import ComponentMap from '$lib/ComponentMap.svelte';
+	import { yandexMaps, yandexRoute } from '$lib/scripts/yandexMaps';
 
-	let routeData = {
+	let data = {
 		// Длина маршрута
 		distance: '',
 		// Время прохождения маршрута
 		duration: ''
 	};
 
-	function init() {
-		var myMap = new ymaps.Map('map', {
-			center: [55.751574, 37.573856],
-			zoom: 9,
-			controls: []
-		});
-
-		// Создание экземпляра маршрута.
-		var multiRoute = new ymaps.multiRouter.MultiRoute(
-			{
-				// Точки маршрута.
-				// Обязательное поле.
-				referencePoints: [
-					'Россия, Бабынино, улица Комсомольская, 39',
-					'Россия, Калуга, улица Вилонова, 1'
-				],
-				// Выбор маршрута с учётом пробок
-				params: {
-					avoidTrafficJams: true
-				}
-			},
-			{
-				// Автоматически устанавливать границы карты так,
-				// чтобы маршрут был виден целиком.
-				boundsAutoApply: true
-			}
-		);
-
-		// Добавление маршрута на карту.
-		myMap.geoObjects.add(multiRoute);
-
-		// Подписка на событие обновления данных маршрута.
-		// Обратите внимание, подписка осуществляется для поля model.
-		multiRoute.model.events.add('requestsuccess', function () {
-			// Получение ссылки на активный маршрут.
-			var activeRoute = multiRoute.getActiveRoute();
-			// Вывод информации о маршруте.
-			routeData.distance = activeRoute.properties.get('distance').text;
-			routeData.duration = activeRoute.properties.get('duration').text;
-			console.log('Длина: ' + activeRoute.properties.get('distance').text);
-			console.log('Время прохождения: ' + activeRoute.properties.get('duration').text);
-		});
-	}
-
 	onMount(() => {
-		ymaps.ready(init);
+		yandexMaps().then((maps) => {
+			let route = yandexRoute(
+				'Россия, Бабынино, улица Комсомольская, 39',
+				'Россия, городской округ Калуга, деревня Мстихино'
+			);
+
+			// Подписка на событие обновления данных маршрута.
+			// Обратите внимание, подписка осуществляется для поля model.
+			route.model.events.add('requestsuccess', function () {
+				// Получение ссылки на активный маршрут.
+				let activeRoute = route.getActiveRoute();
+				// Вывод информации о маршруте.
+				data.distance = activeRoute.properties.get('distance').text;
+				data.duration = activeRoute.properties.get('duration').text;
+			});
+
+			maps.geoObjects.add(route);
+		});
 	});
 </script>
 
@@ -65,6 +39,6 @@
 >
 	<div slot="заголовок" class="alert alert-info small mt-3">
 		<b>Информация о маршруте:</b>
-		<span>длина: {routeData.distance}, время прохождения: {routeData.duration}</span>
+		<span>длина: {data.distance}, время прохождения: {data.duration}</span>
 	</div>
 </ComponentMap>
