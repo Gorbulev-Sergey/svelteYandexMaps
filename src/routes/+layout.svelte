077ad608200617c6a901841a853585c2bbc29d19
Горбулёв Сергей <gorbulevsv@yandex.ru>
2022-09-26 15:32:46 +0300

    1

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,22 +9,26 @@
 			<span class="navbar-toggler-icon" />
 		</button>
 		<div class="collapse navbar-collapse" id="navbarNav">
-			<ul class="navbar-nav">
-				<li class="nav-item">
-					<a class="nav-link" href="/">Домой</a>
-				</li>
-				<li class="nav-item">
-					<a class="nav-link" href="/distance/betweentwopoints">Между двумя точками</a>
-				</li>
-				<li class="nav-item">
-					<a class="nav-link" href="/searchlist">Поиск данных</a>
-				</li>
-				<li class="nav-item">
-					<a class="nav-link" href="https://developer.tech.yandex.ru/services/" target="blank"
-						>Кабинет разработчика</a
-					>
-				</li>
-			</ul>
+			<div class="d-flex align-items-start justify-content-between w-100">
+				<ul class="navbar-nav">
+					<li class="nav-item">
+						<a class="nav-link" href="/">Домой</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="/distance/betweentwopoints">Между двумя точками</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="/searchlist">Поиск данных</a>
+					</li>
+				</ul>
+				<ul class="navbar-nav">
+					<li class="nav-item">
+						<a class="nav-link" href="https://developer.tech.yandex.ru/services/" target="blank"
+							>Кабинет разработчика</a
+						>
+					</li>
+				</ul>
+			</div>
 		</div>
 	</div>
 </nav>
